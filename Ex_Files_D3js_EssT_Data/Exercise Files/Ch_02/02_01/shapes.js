fb152212a198bf3fc499964ea6626131d8dbccd2
PaulK <paul.kot@withyouwithme.com>
2022-11-16 10:14:28 -0800

    save

--- Ex_Files_D3js_EssT_Data/Exercise Files/Ch_02/02_01/shapes.js
+++ Ex_Files_D3js_EssT_Data/Exercise Files/Ch_02/02_01/shapes.js
@@ -1,1 +1,21 @@
+//Create data
+var dataArray = [5, 11,18];
 
+//Add SVG element to HTML page
+//svg is a canvas that we add lines and text
+
+
+//Change width and hight with attributes
+// d3.select("body").append("svg")
+//     .attr("height","100%")
+//     .attr("width","100%");
+
+//Create short cut to make life easier. set d3 to a variable.
+var svg = d3.select("body").append("svg")
+                .attr("height","100%")
+                .attr("width","100%");
+
+//Add rectangles to svg.
+svg.selectAll("rect") //seach svg to find rectangles
+        .data(dataArray)
+        .enter().append("rect");
